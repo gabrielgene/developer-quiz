8d728f0a756be22e562da1465d4bc41ea7e0c0ed
Gabriel Gene <gabrielgene97@gmail.com>
2021-05-30 21:25:19 -0300

    Rename stripQuotes function

--- stripquote/index.test.js
+++ stripquote/index.test.js
@@ -1,21 +1,21 @@
-const removeQuotes = require('./');
+const stripQuotes = require('./');
 
 test('remove the quotes as expected', () => {
   expect(
-    removeQuotes(`"I'm" a setence "with' 'simple quote' "and" "double quote"`)
+    stripQuotes(`"I'm" a setence "with' 'simple quote' "and" "double quote"`)
   ).toBe(`I'm a setence with simple quote and double quote`);
 });
 
 test('remove unicode quotes as expected', () => {
   expect(
-    removeQuotes(
+    stripQuotes(
       `'quotation' "quotation" ’quotation´ ‘quotation’ ‘quotation’ “quotation”`
     )
   ).toBe(`quotation quotation quotation quotation quotation quotation`);
 });
 
 test('handle with unexpected parameters', () => {
-  expect(() => removeQuotes(0)).toThrowError(/not a string/);
-  expect(() => removeQuotes(null)).toThrowError(/not a string/);
-  expect(removeQuotes()).toBe('');
+  expect(() => stripQuotes(0)).toThrowError(/not a string/);
+  expect(() => stripQuotes(null)).toThrowError(/not a string/);
+  expect(stripQuotes()).toBe('');
 });
