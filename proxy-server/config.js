c9d954d3ffea30daa00e754c803da3594a6edce5
Gabriel Gene <gabrielgene97@gmail.com>
2021-06-01 18:35:45 -0300

    Change to https

--- proxy-server/config.js
+++ proxy-server/config.js
@@ -3,8 +3,8 @@ const logger = require('./plugins/logger');
 const config = {
   port: 3000,
   upstream: {
-    pokeapi: 'http://pokeapi.co/api/v2',
-    default: 'http://pokeapi.co/api/v2',
+    pokeapi: 'https://pokeapi.co/api/v2',
+    default: 'https://pokeapi.co/api/v2',
     hookbin: 'https://hookb.in/3OZj819xd0HEwwjBl0za',
   },
   beforeRequestPlugins: [logger(console.log)],
